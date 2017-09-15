35f85cc8ddab9066a5707fe2e0e5852e2b93ea02
Marcin <Marcin@WIN-2D632A196CF>
2017-09-15 10:53:34 +0200

    added version tag

--- src/game/TicTacToe.java
+++ src/game/TicTacToe.java
@@ -1,6 +1,7 @@
 package game;
 
 public class TicTacToe {
+	public static final String version = "1.00.00";
 
 	public static void main(String[] args) {
 		java.awt.EventQueue.invokeLater(new Runnable() {
